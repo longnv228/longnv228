c766648b97e8a61110f71cf1acddc6cf4c0ef0ae
longnv228 <longnv2.vtb@gmail.com>
2023-05-02 09:13:11 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+7.5 % 3.5
+123 / 0
+10 * (5 + 3)
+10 * 5 + 3
+print('Sum is', 10 +9)
+print("""Display name is  O' Brien""")
+triple_quoted_string = """This is a triple - quoted 
+string that spans two lines"""
+triple_quoted_string
+chuoi_ky_tu = """Day la cau 2 hang"""
+chuoi_ky_tu
+
+print("""This is a lengthy
+multiline string containing \
+a few lines \
+of text""")
+print("""Nguyen \
+Viet Long""")
+
+name = input("What's your name?")
+name
+print("My name is", name)
+
+value1 = input("Enter first name")
+value2 =  input(" Enter last name")
+print(value1, value2)
+value3 = int(input("Enter an interger"))
+print("Your interger is", value3)
